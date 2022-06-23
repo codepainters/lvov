593b4376a3951cc238be94f745586f0c67377473
Przemyslaw Wegrzyn <pwegrzyn@codepainters.com>
2022-06-23 08:07:25 +0200

    Test: fixed start sequence, it's necessary to jump first.

--- tests/01_hello_pio.asm
+++ tests/01_hello_pio.asm
@@ -2,16 +2,20 @@
 ; Simple test toggling PA[7..0] outputs endlessly, avoiding RAM access.
 ;
 
-; Note: at reset ROM is mapped at address 0 and gets remapped after first I/O write
+; Note: at reset ROM is mapped 4 times at 0, 0x4000, 0x8000 and 0xC000 
+; and gets remapped after first I/O write. Execution starts at 0,
+; so we first have to jump to change the PC to 0xC..., then we can issue
+; first I/O write.
 
 .org    0xC000
+
+    JP      l1
+l1: NOP
 
     ; configure 8255 - all outputs, mode 0
     LD      A, 0x80
     OUT     (0xC3), A
-    NOP
-    NOP
-        
+
 loop:
     LD      A, 0xAA
     OUT     (0xC0), A
