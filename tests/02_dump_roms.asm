e3c2981dad75d1df9d5ab8b86c9c73acc15031a1
Przemyslaw Wegrzyn <pwegrzyn@codepainters.com>
2022-07-01 20:26:52 +0200

    Test 02: dump ROMs via PIO port

---
+++ tests/02_dump_roms.asm
@@ -0,0 +1,36 @@
+; 
+; Dump ROM area via PIO. 
+;
+
+.org    0xC000
+
+    JP      l1
+l1: NOP
+
+    ; configure 8255 - all outputs, mode 0, set PB2 = 0
+    LD      A, 0x80
+    OUT     (0xC3), A
+    LD      A, 0x00
+    OUT     (0xC1), A
+    
+
+    LD      HL, 0xC000      ; ROM starts at 0xC000
+
+loop:
+    LD      A, (HL)
+    OUT     (0xC0), A
+
+    ; pulse strobe (PB2)
+    LD      A, 0x04
+    OUT     (0xC1), A
+    LD      A, 0x00
+    OUT     (0xC1), A
+    NOP
+    NOP   
+ 
+    INC     HL              
+    JP      NZ, loop        ; repeat until wraps around 
+
+    ; endless loop when done
+done:
+    JP      done
